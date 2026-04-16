064533bd3b7f0713aaa003b95f9d808a6fe53900
carolinaFerreira1 <caaroferreira120903@gmail.com>
2026-04-16 11:55:44 -0300

    Ejercicio 1: variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables-y-operaciones/app.js
@@ -0,0 +1,16 @@
+const nombre = "Carolina Ferreira"; 
+const edad = 22; 
+const materia = "Desarrollo de Software";
+
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+
+let contador = 0;
+
+
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+
+console.log("Valor final del contador:", contador);
